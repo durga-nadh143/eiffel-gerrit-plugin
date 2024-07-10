5172bc153dead2ec73367951b9efa19937cc947f
durga-nadh143 <a.durganadh@tcs.com>
2024-07-10 17:10:53 +0530

    Update CommitInformation.java removed try catch block

--- src/main/java/com/ericsson/gerrit/plugins/eiffel/git/CommitInformation.java
+++ src/main/java/com/ericsson/gerrit/plugins/eiffel/git/CommitInformation.java
@@ -98,17 +98,17 @@ public class CommitInformation {
     private List<RevCommit> getParentsFromCommit(final String commitId, final String projectName)
        throws UnprocessableEntityException, IOException, ResourceNotFoundException, PermissionBackendException {
           
-        try{
+        //try{
         final ProjectResource projectResource = projectsCollection.parse(projectName, true);
         final CommitResource commitResource = commitsCollection.parse(projectResource,
                 IdString.fromDecoded(commitId));
         final RevCommit commit = commitResource.getCommit();
         final RevCommit[] parents = commit.getParents();
             return Arrays.asList(parents);
-           }
-        catch (PermissionBackendException e) {
-           System.out.println("PermissionBackendException is occured");
-        }
+          // }
+        // catch (PermissionBackendException e) {
+        //    System.out.println("PermissionBackendException is occured");
+        // }
            
        
         
